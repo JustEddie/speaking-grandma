b58432c74e0d733babc981a4b6965aa5d3e99612
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-08 23:19:31 +1000

    commit tree 465bbad2590cd23282996e44076315984cb7f204

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(say)
+    
+    if say.upcase != say
+        grandma = "HUH?! SPEAK UP, SONNY!"
+    elsif say == "I LOVE YOU GRANDMA!"
+        grandma = "I LOVE YOU TOO PUMPKIN!"
+    elsif say.upcase == say
+        grandma = "NO, NOT SINCE 1938!"
+end
+grandma
+end
